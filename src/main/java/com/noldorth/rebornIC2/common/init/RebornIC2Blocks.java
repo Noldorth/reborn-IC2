450d247466f415ded44b23b52e71ac420b9ad48b
Baptiste Célérier <celerier.baptiste1@gmail.com>
2021-06-02 15:44:37 +0200

    idem

--- src/main/java/com/noldorth/rebornIC2/common/init/RebornIC2Blocks.java
+++ src/main/java/com/noldorth/rebornIC2/common/init/RebornIC2Blocks.java
@@ -1,2 +1,30 @@
-package com.noldorth.rebornIC2.common.init;public class rebornIC2Blocks {
+package com.noldorth.rebornIC2.common.init;
+
+import com.noldorth.rebornIC2.common.RebornIC2;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.function.Supplier;
+
+public class RebornIC2Blocks {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, RebornIC2.MODID);
+
+    //public static final RegistryObject<Block> COPPER_ORE = createBlock("copper_ore", () -> new Block(AbstractBlock.Properties.create(Material.ROCK).hardnessAndResistance(3f, 15).harvestTool(ToolType.PICKAXE).harvestLevel(2).setRequiresTool()));
+
+    public static final RegistryObject<Block> COPPER_BLOCK = createBlock("copper_block", () -> new Block (AbstractBlock.Properties.create(Material.IRON).hardnessAndResistance(3f, 15).harvestTool(ToolType.PICKAXE).harvestLevel(1).setRequiresTool()));
+
+
+
+    public static RegistryObject<Block> createBlock(String name, Supplier<? extends Block> supplier) {
+        RegistryObject<Block> block = BLOCKS.register(name, supplier);
+        RebornIC2Items.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+        return block;
+    }
 }
--- src/main/java/com/noldorth/rebornIC2/common/init/RebornIC2Items.java
+++ src/main/java/com/noldorth/rebornIC2/common/init/RebornIC2Items.java
@@ -1,2 +1,20 @@
-package com.noldorth.rebornIC2.common.init;public class rebornIC2Items {
+package com.noldorth.rebornIC2.common.init;
+
+import com.noldorth.rebornIC2.common.RebornIC2;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class RebornIC2Items {
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, RebornIC2.MODID);
+
+    public static final RegistryObject<Item> INGOT_COPPER = ITEMS.register("ingot_copper",() -> new Item(new Item.Properties().group(ItemGroup.MATERIALS)));
+
+    //public static final
+    //public static final RegistryObject<Item> COPPER_ORE_ITEM = ITEMS.register("copper_ore_item",() -> new Item(new Item.Properties()));
+
+
 }
