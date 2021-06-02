a92f15634afc31de08c5e65d509b9adba7d3c4ec
Baptiste Célérier <celerier.baptiste1@gmail.com>
2021-06-02 15:44:14 +0200

    premier block ready (copper block et lootable) Probleme de texture quand on le pose

--- src/main/java/com/noldorth/rebornIC2/common/RebornIC2.java
+++ src/main/java/com/noldorth/rebornIC2/common/RebornIC2.java
@@ -1,21 +1,28 @@
-package com.noldorth.rebornIC2;
+package com.noldorth.rebornIC2.common;
 
+import com.noldorth.rebornIC2.common.init.RebornIC2Blocks;
+import com.noldorth.rebornIC2.common.init.RebornIC2Items;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(RIC2.MODID)
-public class RIC2 {
+@Mod(RebornIC2.MODID)
+public class RebornIC2 {
     public static final String MODID = "rebornic2";
 
     private void setup(FMLCommonSetupEvent e) {
 
     }
 
-    public RIC2() {
+    public RebornIC2() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
+
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        RebornIC2Items.ITEMS.register(bus);
+        RebornIC2Blocks.BLOCKS.register(bus);
     }
 
     private void clientSetup(FMLClientSetupEvent e) {
---
+++ src/main/java/com/noldorth/rebornIC2/common/init/RebornIC2Items.java
@@ -0,0 +1,2 @@
+package com.noldorth.rebornIC2.common.init;public class rebornIC2Items {
+}
